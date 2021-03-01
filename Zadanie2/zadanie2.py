63ffa368b9e3373ebdfab464d03e96da65454b2a
EfRy EfRy <jakubtrstensky@gmail.com>
2021-03-01 17:02:21 +0100

    feat: create basic skeleton of Fibannoci

---
+++ Zadanie2/zadanie2.py
@@ -0,0 +1,32 @@
+from fei.ppds import Event
+from random import randint as rand
+from fei.ppds import Thread, Semaphore, Mutex
+from time import sleep
+from fei.ppds import print
+
+
+class Fibannoci:
+    def __init__(self, numberOfThreads):
+        self.numberOfThreads = numberOfThreads
+        self.actualPosition = 1
+        self.fibannociList = [0, 1]
+        self.semaphoreList = []
+
+
+def start(f, thread_id):
+
+    print("Thread %d done" % thread_id)
+    print(f.fibannociList)
+
+
+numberOfThreads = 20
+f = Fibannoci(numberOfThreads)
+
+threads = list()
+
+for i in range(numberOfThreads):
+    t = Thread(start, f, i)
+    threads.append(t)
+
+for t in threads:
+    t.join()
