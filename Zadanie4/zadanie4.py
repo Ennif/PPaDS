29b39503fa62eb4c81db7af9983509f30e26fe6b
EfRy EfRy <jakubtrstensky@gmail.com>
2021-03-15 13:49:16 +0100

    feat: implement LightSwitch class

---
+++ Zadanie4/zadanie4.py
@@ -0,0 +1,26 @@
+import fei.ppds as fp
+from fei.ppds import Mutex, Semaphore, print, Thread, Event
+from time import sleep
+from random import randint
+
+
+class Lightswitch:
+    def __init__(self):
+        self.counter = 0
+        self.mutex = Mutex()
+
+    def lock(self, semaphore):
+        self.mutex.lock()
+        tmp_counter = self.counter
+        self.counter += 1
+        if self.counter == 1:
+            semaphore.wait()
+        self.mutex.unlock()
+        return tmp_counter
+
+    def unlock(self, semaphore):
+        self.mutex.lock()
+        self.counter -= 1
+        if self.counter == 0:
+            semaphore.signal()
+        self.mutex.unlock()
