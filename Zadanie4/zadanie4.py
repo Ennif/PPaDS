9d6577f35d30dcc7dd7e49053bdc454ef83b3513
EfRy EfRy <jakubtrstensky@gmail.com>
2021-03-15 21:38:31 +0100

    style: formatted code by PEP8 rules

--- Zadanie4/zadanie4.py
+++ Zadanie4/zadanie4.py
@@ -27,55 +27,78 @@ class Lightswitch:
 
 
 class Operator:
-    def __init__(self,ls_operators,without_operators,without_sensors,simple_barrier):
-        self.lightswitch_operators = ls_operators
+    def __init__(self,
+                 ls_operators,
+                 without_operators,
+                 without_sensors,
+                 simple_barrier
+                 ):
+        self.ls_ops = ls_operators
         self.without_operators = without_operators
         self.without_sensors = without_sensors
         self.simple_barrier = simple_barrier
 
-    def operator(self,operator_id):
+    def operator(self, operator_id):
         while True:
             self.simple_barrier.wait()
             self.without_operators.wait()
-            number_reading_operators = self.lightswitch_operators.lock(self.without_sensors)
+            num_reading_ops = self.ls_ops.lock(self.without_sensors)
             self.without_operators.signal()
             waiting_time_of_operator = randint(4, 5)/100
-            print('monit "%02d": pocet_citajucich_monitorov=%02d, trvanie_citania=%0.3f\n' % (operator_id,number_reading_operators,waiting_time_of_operator))
+            print(
+                'monit "d": pocet_citajucich_monitorov=d,'
+                "trvanie_citania=%0.3f\n"
+                % (operator_id, num_reading_ops, waiting_time_of_operator)
+            )
             sleep(waiting_time_of_operator)
-            self.lightswitch_operators.unlock(self.without_sensors)
+            self.ls_ops.unlock(self.without_sensors)
 
 
 class Sensor:
-    def __init__(self,ls_sensors,w_data_by_sens,without_sensors,without_operators,simple_barrier):
-        self.lightswitch_sensors = ls_sensors
+    def __init__(self,
+                 ls_sensors,
+                 w_data_by_sens,
+                 without_sensors,
+                 without_operators,
+                 simple_barrier
+                 ):
+        self.ls_sens = ls_sensors
         self.written_data_by_sensors = w_data_by_sens
         self.without_sensors = without_sensors
         self.without_operators = without_operators
         self.simple_barrier = simple_barrier
 
-    def sensor_P_T(self,sensor_id):
+    def sensor_P_T(self, sensor_id):
         while True:
-            sleep(randint(5,6)/100)
-            number_writing_sensors = self.lightswitch_sensors.lock(self.without_operators)
+            sleep(randint(5, 6)/100)
+            num_writing_sens = self.ls_sens.lock(self.without_operators)
             self.without_sensors.wait()
             waiting_time_of_sensor = randint(1, 2)/100
-            print('cidlo "%02d": pocet_zapisujucich_cidiel=%02d, trvanie_zapisu=%0.3f\n' % (sensor_id,number_writing_sensors,waiting_time_of_sensor))
+            print(
+                'cidlo "d": pocet_zapisujucich_cidiel=d,'
+                "trvanie_zapisu=%0.3f\n"
+                % (sensor_id, num_writing_sens, waiting_time_of_sensor)
+            )
             sleep(waiting_time_of_sensor)
             self.without_sensors.signal()
             self.simple_barrier.wait()
-            self.lightswitch_sensors.unlock(self.without_operators)
+            self.ls_sens.unlock(self.without_operators)
 
     def sensor_H(self, sensor_id):
         while True:
-            sleep(randint(5,6)/100)
-            number_writing_sensors = self.lightswitch_sensors.lock(self.without_operators)
+            sleep(randint(5, 6)/100)
+            num_writing_sens = self.ls_sens.lock(self.without_operators)
             self.without_sensors.wait()
             waiting_time_of_sensor = randint(20, 25)/1000
-            print('cidlo "%02d": pocet_zapisujucich_cidiel=%02d, trvanie_zapisu=%0.3f\n' % (sensor_id,number_writing_sensors,waiting_time_of_sensor))
+            print(
+                'cidlo "d": pocet_zapisujucich_cidiel=d,'
+                "trvanie_zapisu=%0.3f\n"
+                % (sensor_id, num_writing_sens, waiting_time_of_sensor)
+            )
             sleep(waiting_time_of_sensor)
             self.without_sensors.signal()
             self.simple_barrier.wait()
-            self.lightswitch_sensors.unlock(self.without_operators)
+            self.ls_sens.unlock(self.without_operators)
 
 
 class PowerStation:
@@ -86,18 +109,17 @@ class PowerStation:
         self.without_operators = Semaphore(1)
         self.without_sensors = Semaphore(1)
         self.simple_barrier = SimpleBarrier(3)
-        self.operator = Operator(
-            self.lightswitch_operators,
-            self.without_operators,
-            self.without_sensors,
-            self.simple_barrier
-            )
-        self.sensor = Sensor(
-            self.lightswitch_sensors,
-            self.written_data_by_sensors,
-            self.without_sensors,
-            self.without_operators,
-            self.simple_barrier)
+        self.operator = Operator(self.lightswitch_operators,
+                                 self.without_operators,
+                                 self.without_sensors,
+                                 self.simple_barrier
+                                 )
+        self.sensor = Sensor(self.lightswitch_sensors,
+                             self.written_data_by_sensors,
+                             self.without_sensors,
+                             self.without_operators,
+                             self.simple_barrier
+                             )
 
 
 class SimpleBarrier:
@@ -119,12 +141,12 @@ class SimpleBarrier:
 threads = []
 power_station = PowerStation()
 
-threads.append(Thread(power_station.sensor.sensor_H,0))
-threads.append(Thread(power_station.sensor.sensor_P_T,1))
-threads.append(Thread(power_station.sensor.sensor_P_T,2))
+threads.append(Thread(power_station.sensor.sensor_H, 0))
+threads.append(Thread(power_station.sensor.sensor_P_T, 1))
+threads.append(Thread(power_station.sensor.sensor_P_T, 2))
 
 for i in range(8):
-    t = Thread(power_station.operator.operator,i)
+    t = Thread(power_station.operator.operator, i)
     threads.append(t)
 
 for t in threads:
