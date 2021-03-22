e7fce491e1c4910f8384f9466ee1f66aad7eb46b
EfRy EfRy <jakubtrstensky@gmail.com>
2021-03-22 16:01:16 +0100

    feat: implement SimpleBarrier class

---
+++ Zadanie5/zadanie5.py
@@ -0,0 +1,29 @@
+from fei.ppds import Semaphore, Mutex, Thread, print
+from random import randint
+from time import sleep
+
+
+class SimpleBarrier:
+
+    def __init__(self, N):
+        self.N = N
+        self.mutex = Mutex()
+        self.cnt = 0
+        self.sem = Semaphore(0)
+
+    def wait(self,
+             print_str,
+             savage_id,
+             print_last_thread=False,
+             print_each_thread=False):
+        self.mutex.lock()
+        self.cnt += 1
+        if print_each_thread:
+            print(print_str % (savage_id, self.cnt))
+        if self.cnt == self.N:
+            self.cnt = 0
+            if print_last_thread:
+                print(print_str % (savage_id))
+            self.sem.signal(self.N)
+        self.mutex.unlock()
+        self.sem.wait()
