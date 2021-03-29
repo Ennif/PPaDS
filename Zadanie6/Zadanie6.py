bee7984249e3c3035c67321169592e0ce06849cf
EfRy EfRy <jakubtrstensky@gmail.com>
2021-03-29 15:24:20 +0200

    docs: add comments to code
    every important part of code has its own comment

--- Zadanie6/Zadanie6.py
+++ Zadanie6/Zadanie6.py
@@ -1,5 +1,16 @@
 """
 Vypracovanie ulohy - tvorba molekul vody
+Je potrebna implementacia znovupouzitelnej bariery
+inicializovanej na hodnotu 3 (2 atomy vodika,
+1 atom kyslika). Bariera nam zabezpeci to,
+aby sa pockali tieto tri atomy a zamadzili tak
+tvorbe dalsej molekuly, kym sa nezlucia aktualne
+tri atomy. Taktiez sa pouzil binarny semafor.
+Binarny preto, lebo chceme aby ine vlakno uvolnilo
+chod kritickej oblasti. V pripade pouzitia mutexu
+by jedno vlakno, ktore zamklo oblast muselo tuto
+oblast aj odokmnut, co je pre tento problem
+nepouzitelne.
 """
 from fei.ppds import Semaphore, Mutex, Thread, Event, print
 from random import randint
@@ -26,6 +37,7 @@ class Barrier:
 
 class Molecule:
     def __init__(self):
+        # binarny semafor
         self.mutex = Semaphore(1)
         self.oxygenQueue = Semaphore(0)
         self.hydrogenQueue = Semaphore(0)
@@ -34,40 +46,75 @@ class Molecule:
         self.barrier = Barrier(3)
 
     def oxygenFunc(self):
+            # zamadzenie pristupu do KO
             self.mutex.wait()
             self.oxygen += 1
+            """
+            Ak je este nedostatocny pocet vodikov
+            tak chod dalej a uvolni pridanie
+            sa dalsiemu kysliku
+            """
             if self.hydrogen < 2:
                 self.mutex.signal()
             else:
                 self.oxygen -= 1
                 self.hydrogen -= 2
+                # odobera sa kyslik z "fronty"
                 self.oxygenQueue.signal()
+                # odoberaju sa 2 vodiky z "fronty"
                 self.hydrogenQueue.signal(2)
 
+            # Pridava sa jeden kyslik do "fronty"
             self.oxygenQueue.wait()
             print("oxygen do zlucenia")
+
+            # zlucovanie
             self.bond()
+
+            """
+            bariera, kde sa caka na dobehnutie
+            vsetkych 3 atomov po zluceni, aby
+            sa mohli ist dalsie atomy zlucovat
+            """
             self.barrier.wait()
             self.mutex.signal()
 
     def hydrogenFunc(self):
+        # zamadzenie pristupu do KO
         self.mutex.wait()
         self.hydrogen += 1
+
+        """
+        Ak je nedostatocny pocet kyslikov alebo vodikov
+        tak uvolni pridanie sa dalsiemu vodiku a chood
+        dalej
+        """
         if self.hydrogen < 2 or self.oxygen < 1:
             self.mutex.signal()
         else:
             self.oxygen -= 1
             self.hydrogen -= 2
+            # odobera sa kyslik z "fronty"
             self.oxygenQueue.signal()
+            # odoberaju sa 2 vodiky z "fronty"
             self.hydrogenQueue.signal(2)
 
+        # Pridava sa vodik do zlucenia
         self.hydrogenQueue.wait()
         print("Hydrogen do zlucenia")
+
+        # zlucovanie
         self.bond()
 
+        """
+        bariera, kde sa caka na dobehnutie
+        vsetkych 3 atomov po zluceni, aby
+        sa mohli ist dalsie atomy zlucovat
+        """
         self.barrier.wait()
 
     def bond(self):
+        # prevedenie zlucovania
         sleep(randint(5, 15)/10)
         print("Zlucenie buniek")
 
@@ -76,6 +123,8 @@ def init_and_run():
 
     threads = list()
     molecule = Molecule()
+
+    # neustale sa tvoriace vlakna vodikov a kyslikov
     while True:
         threads.append(Thread(molecule.oxygenFunc))
         threads.append(Thread(molecule.hydrogenFunc))
