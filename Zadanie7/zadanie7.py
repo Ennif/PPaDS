53523773361c006b3322a25885263320cc6e9323
EfRy EfRy <jakubtrstensky@gmail.com>
2021-04-12 23:53:54 +0200

    feat: implement Task class wrapper to coroutine

--- Zadanie7/zadanie7.py
+++ Zadanie7/zadanie7.py
@@ -3,6 +3,19 @@ def coroutine():
         task_id = (yield)
         print("Called coroutine by task ID: ", task_id)
 
+
+class Task(object):
+    id = 0
+
+    def __init__(self, coroutine):
+        Task.id += 1
+        self.task_id = Task.id
+        self.coroutine = coroutine
+
+    def run_task(self, attr):
+        return self.coroutine.send(attr)
+
+
 if __name__ == "__main__":
 
     coroutine()
